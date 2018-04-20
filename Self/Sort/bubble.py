3a689b3f6f7c924efdb0180dc8b469000f827758
tatatakky <s1240232@gmail.com>
2018-04-21 00:49:53 +0900

    [add] bubble-sort

--- Self/Sort/bubble.py
+++ Self/Sort/bubble.py
@@ -3,9 +3,11 @@
 
 def bubble(arr):
     for i in range(len(arr)):
-        for j in range(len(arr)-1,i,-1):
-            if arr[j-1] >= arr[j]:
+        for j in range(len(arr)-1,i,-1): #10からiまで1ずつ減る
+            # print(j-1,j)
+            if arr[j-1] > arr[j]:
                 arr[j-1],arr[j]=arr[j],arr[j-1]
+            print(arr)
     return arr
 
 li=[9,1,6,4,9,2,7,4,5,3,8]
